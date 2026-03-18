49be0504eea29d354f0a0b58791728271b79dcea
Luciano Ramos <lramosalves37@gmail.com>
2026-03-18 16:52:35 -0300

    creat data-tpye

---
+++ programming logic/data-type/data-type.py
@@ -0,0 +1,11 @@
+
+a = 10 #int
+b = 2.5 #float
+c = 3 + 90j #complex
+d = "olá" #string
+#boll
+e = True
+f = False
+
+g = None #ainda vazio
+h = "" #vazio , ainda vou declarar mas , esse é do tipo string
