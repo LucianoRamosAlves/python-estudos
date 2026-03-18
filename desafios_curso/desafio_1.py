640f5163b736dd8380327e467d6d4db06b889654
Luciano Ramos <lramosalves37@gmail.com>
2026-03-18 16:19:08 -0300

    creat arquivo inputy.py

--- desafios_curso/desafio_1.py
+++
@@ -1,3 +0,0 @@
-nome = input('digite seu nome').strip().capitalize()
-print(f'olá {nome}, seja Bem vido ao clube!')
-#@ desafio extra, posso fazer um questionario perguntando se é feminino ou mascculino, e só depois exibir a mensagem com um imojim de mulher ouo homen
---
+++ desafios_curso/introdution.py
@@ -0,0 +1,3 @@
+nome = input('digite seu nome').strip().capitalize()
+print(f'olá {nome}, seja Bem vido ao clube!')
+#@ desafio extra, posso fazer um questionario perguntando se é feminino ou mascculino, e só depois exibir a mensagem com um imojim de mulher ouo homen
