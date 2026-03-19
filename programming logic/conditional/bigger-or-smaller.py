cc94046b30b13cb51bb6a949f063863b0cbfbff5
Luciano Ramos <lramosalves37@gmail.com>
2026-03-19 20:49:26 -0300

    creat bigger-or-snaller

---
+++ programming logic/conditional/bigger-or-smaller.py
@@ -0,0 +1,9 @@
+n1 =  float(input("digite o primeiro  numero"))
+n2 =  float(input("digite o segundo  numero"))
+
+if n1 > n2:
+    print("o primeiro numero é maior")
+elif n1 < n2:
+    print("o segundo numero foi maior")
+else:
+    print("numeros iguais")
