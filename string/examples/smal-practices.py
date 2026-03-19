65b4c9c97597eb51b23f3c128efb458348112f89
Luciano Ramos <lramosalves37@gmail.com>
2026-03-19 11:44:57 -0300

    creat small-practices

---
+++ string/examples/smal-practices.py
@@ -0,0 +1,9 @@
+frase = "Eu gosto de vitaminas principalmente com abacate e banana"
+
+frase_palavras = frase.replace(" ", "")
+primara_palavra = print(f"as 3 primeiras letras {frase_palavras[:3]}")
+
+primara_palavra = print(f"as 3 primeiras letras {frase_palavras[-3:]}")
+
+count_ana = print(f"a palavra ana aparece {frase_palavras.count("ana")} vezes")
+
