3a3db56afe345d8432c110526ebc07869c5549d3
Luciano Ramos <lramosalves37@gmail.com>
2026-03-21 13:14:54 -0300

    creat nested-loop

---
+++ loops/concepts/nested-loop.py
@@ -0,0 +1,9 @@
+# agora eu quero que todod os intens ficam com todos os intens
+#| todas as combinações possiveis
+
+roupas = ['calça', 'camisa', 'cueca']
+tamanhos = ['P', 'M', 'G']
+
+for roupa in roupas:
+    for tamanho in tamanhos:
+        print(f" {roupa} tamamho {tamanho}")
