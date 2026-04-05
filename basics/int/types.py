9bf39b83bf9ab21691f24447417509b4e9bb4884
Luciano Ramos <lramosalves37@gmail.com>
2026-04-05 15:28:50 -0300

    creat type

---
+++ basics/int/types.py
@@ -0,0 +1,6 @@
+print(int('      -34_345'))
+
+# O resultado é -34345, pois o Python ignora os espaços em branco e os underscores, e interpreta o número como um inteiro negativo.
+
+print(int('FACE', base=16))
+# O resultado é 64206, pois 'FACE' é interpretado como um número hexadecimal.
