f9f0cbb1d0b4024d68154a321f47a2fee71d0c93
Luciano Ramos <lramosalves37@gmail.com>
2026-04-18 23:48:13 -0300

    update

--- projects/caixa/caixa.py
+++ projects/caixa/caixa.py
@@ -17,3 +17,69 @@ def troco(valor):
     else:
         return print("saldo no caixa insuficiente.")
     return caixa
+
+def entrada(valor):
+    global caixa
+    if valor > 0:
+        caixa += valor
+    else:
+        return print("numero invalido.")
+    return caixa
+
+opcao = ''
+nome = input('Nome do operador: ').capitalize()
+while opcao != "5":
+
+    print(""" =-=-= CAIXA =-=-
+    [0]. Verificar saldo
+    [1]. Registrar dívida
+    [2]. Registrar troco
+    [3]. Registrar entrada
+    [4]. Fechar caixa
+    [5]. Fechar Programa
+    """)
+
+    opcao = input("Digite uma opção (0-5) ou 'fechar' para encerrar: ").lower()
+    status = ''
+
+    try:
+        if opcao == '4':
+            opcao_fechar = input('Deseja realmente fechar o caixa? (s/n) ').lower()
+            if opcao_fechar == 's':
+                if caixa > 1000:
+                    status = 'BOM'
+                elif caixa > 500:
+                    status = 'REGULAR'
+                else:
+                    status = 'RUIM'
+                with open("caixa.txt", "w") as saldo_caixa:
+                    saldo_caixa.write(f"Responsavél: {nome}\n data: {data} saldo: R$: {caixa}\n situação {status}")
+                    print('Caixa finalizado')
+                    print(f'Responsavél {nome}')
+                    break
+            else:
+                print('Caixa ainda aberto!')
+                continue
+    except:
+        print('opção invalida')
+        print('tente novamente')
+
+    else:
+        if opcao == '0':
+            print(f'Saldo atual: R$: {caixa}')
+        elif opcao == '1':
+            divida(valor := float(input('valor da divida: ')))
+            print('Aplicação da dívida registrada com sucesso!')
+        elif opcao == '2':
+            troco(valor := float(input('valor do troco : ')))
+            print('Aplicação do troco registrada com sucesso!')
+        elif opcao == '3':
+            entrada(valor := float(input('valor da entrada : ')))
+            print('Aplicação da entrada registrada com sucesso!')
+        else:
+            print('opção invalida')
+            print('tente novamente')
+            continue
+else:
+    print('Caixa ainda aberto!')
+
