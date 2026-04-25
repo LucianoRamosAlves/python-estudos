e6bbcc1bda9c5f884b7bdc5eb89f4c2aa783a105
Luciano Ramos <lramosalves37@gmail.com>
2026-04-25 16:57:52 -0300

    creat print

---
+++ library/rich/print.py
@@ -0,0 +1,7 @@
+from rich import print as rich_print
+
+listas = [1, 2, 3, 4, 5, 3.5, "Hola", True]
+dicionario = {"nombre": "Juan", "edad": 30, "ciudad": "Madrid"}
+
+rich_print("Lista:", listas)
+rich_print("Diccionario:", dicionario)
