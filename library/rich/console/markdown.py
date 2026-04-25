db06b0fb8fa7024d16ff8d5652ae79fc1f06f71d
Luciano Ramos <lramosalves37@gmail.com>
2026-04-25 18:54:40 -0300

    creat markdown.py

---
+++ library/rich/console/markdown.py
@@ -0,0 +1,24 @@
+from rich import print as rich_print
+from rich.markdown import Markdown
+
+nome = "Ramon"
+markdown_text = """
+# Título Principal {nome}
+## Subtítulo
+- Item 1
+- Item 2
+```python
+def saudacao():
+    return "Olá, mundo!"
+```     
+meu nome é **Ramon** e eu tenho *30 anos*.
+
+menu - pizza
+- sushi
+1. Item 1
+2. Item 2
+>>> Citação
+
+
+"""
+rich_print(Markdown(markdown_text))
