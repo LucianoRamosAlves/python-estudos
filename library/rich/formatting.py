0a41406a6a2c98607c668c9a221e32b5bf41d712
Luciano Ramos <lramosalves37@gmail.com>
2026-04-25 17:12:51 -0300

    creat  formatting.py

---
+++ library/rich/formatting.py
@@ -0,0 +1,12 @@
+from rich import print as rich_print
+
+rich_print('ola mundo')
+
+rich_print(" meu nome é [b]Ramon[/b]")
+rich_print(" meu nome é [b magenta]Ramon[/b magenta]")
+rich_print(" meu nome é [b magenta]Ramon[/b magenta] eu tenho [b cyan]30 anos[/b cyan]")
+rich_print(" meu nome é [b magenta]Ramon[/b magenta] eu tenho [b cyan]30 anos[/b cyan] e moro em [b yellow]Madrid[/b yellow]")
+
+rich_print(" meu nome é [r]pedro[/r] e eu tenho [s cyan]30 anos[/s cyan]")
+rich_print(" meu nome é [i]pedro[/i] e eu tenho [u red]30 anos[/u red]")
+rich_print(" meu nome é [r]pedro[/r] e eu tenho [blink]30 anos[/blink]")
