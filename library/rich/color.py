47ef06651e2e560506a9c0bdbcd4eda4cfd55df6
Luciano Ramos <lramosalves37@gmail.com>
2026-04-25 17:24:47 -0300

    update color.py

--- library/rich/color.py
+++ library/rich/color.py
@@ -1,3 +1,9 @@
 from rich import print as rich_print
 
 rich_print(" eu goso de [bold] [red] python [/red] ! [/bold]")
+
+rich_print('''
+
+[red on green b  s bold ] python é a melhor linguagem de todas! [/]
+           
+           ''')
