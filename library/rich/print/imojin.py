ed3c252b58c355acbc83be3555300c9e322b77c8
Luciano Ramos <lramosalves37@gmail.com>
2026-04-25 17:38:31 -0300

    creat print and moved

---
+++ library/rich/print/imojin.py
@@ -0,0 +1,8 @@
+from rich import print as rich_print
+
+rich_print("Hola, mundo! a hora :snail: é :clock1: 12:00")
+rich_print("Eu gosto de :pizza: e :sushi:!")
+rich_print("O :dog: é o melhor amigo do :cat:!")
+rich_print("Vamos :rocket: para a lua!")
+rich_print("O :sun_with_face: está brilhando hoje!")
+rich_print("Eu estou me sentindo :smile: hoje!")
