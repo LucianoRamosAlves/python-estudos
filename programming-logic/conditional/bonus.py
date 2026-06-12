04b01bdbf8067e2627d264b0af0f7c669599a453
LucianoRamosAlves <141687028+LucianoRamosAlves@users.noreply.github.com>
2026-06-12 09:18:34 -0300

    create bonus

---
+++ programming-logic/conditional/bonus.py
@@ -0,0 +1,17 @@
+meta = 1000
+f_1 = 1000
+f_2 = 770
+f_3 = 2700
+
+if f_1 >= meta:
+    print(f" o funcionario 1 atingiu a meta com {f_1} vendas bonus de {f_1 * 0.1} reais")
+else:
+    print(f" o funcionario 1 ganhou 0 de bonus")
+if f_2 >= meta:
+    print(f" o funcionario 2 atingiu a meta com {f_2} vendas bonus de {f_2 * 0.1} reais")
+else:
+    print(f" o funcionario 2 ganhou 0 de bonus")
+if f_3 >= meta:
+    print(f" o funcionario 3 atingiu a meta com {f_3} vendas bonus de {f_3 * 0.1} reais")
+else:
+    print(f" o funcionario 3 ganhou 0 de bonus")
