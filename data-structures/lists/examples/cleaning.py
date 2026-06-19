8a90693339c35521ad0b090dd90be6efc820e123
LucianoRamosAlves <141687028+LucianoRamosAlves@users.noreply.github.com>
2026-06-19 14:28:35 -0300

    creat cleaning.py

---
+++ data-structures/lists/examples/cleaning.py
@@ -0,0 +1,111 @@
+gmail_sujos = ['carlos3377@hotmail.com', 'julia1034@gmail.com', 'bruno3028@', 'joao123@gmail.com', 'spam_carlos5610@gmail.com', 'carlos9403@outlook.com', '$$$ana7766@gmail.com', 'lucas4434yahoo.com', 'pedro1753@outlook.com', 'joao123@gmail.com', 'joao123@gmail.com', 'rafael4543hotmail.com', 'joao3510@', 'bruno3107@', 'bruno3494@yahoo.com', 'pedro4401@gmail.com', 'lucas8718@hotmail.com', 'ana4599@hotmail.com', 'spam_ana1821@gmail.com', 'bruno9481@yahoo.com', 'carlos3229@yahoo.com', 'fernanda9945@', 'carlos4356outlook.com', 'ana9264hotmail.com', 'lucas3598@outlook.com', 'maria6451@yahoo.com', 'pedro5364@hotmail.com', 'maria5600@outlook.com', 'spam_ana758@gmail.com', 'joao123@gmail.com', 'joao1868@yahoo.com', 'pedro2719@outlook.com', 'spam_maria7639@gmail.com', 'carlos2646@gmail.com', '$$$fernanda4847@gmail.com', 'fernanda3615yahoo.com', 'rafael930@outlook.com', 'maria9130@outlook.com', '$$$pedro3558@gmail.com', 'bruno5260@gmail.com', '$$$carlos6282@gmail.com', 'lucas1405@', 'julia4340@gmail.com', 'joao5786@outlook.com', '$$$julia2046@gmail.com', 'carlos9727@gmail.com', 'carlos2847@hotmail.com', 'ana3653@hotmail.com', 'bruno1260@yahoo.com', 'rafael450@yahoo.com', 'pedro5769outlook.com', 'ana8106@', 'spam_rafael258@gmail.com', 'fernanda5371@gmail.com', 'bruno385@', 'maria7536@gmail.com', 'joao2060@gmail.com', 'bruno4928@yahoo.com', 'fernanda6802gmail.com', '$$$ana4741@gmail.com', 'pedro9056@hotmail.com', 'joao123@gmail.com', 'maria3965@hotmail.com', 'carlos527@gmail.com', 'joao123@gmail.com', 'ana7035@', 'julia9368@gmail.com', 'rafael1499@gmail.com', 'lucas3953@gmail.com', 'fernanda5109@gmail.com', 'carlos5032@hotmail.com', 'lucas6612outlook.com', 'fernanda2884hotmail.com', 'spam_lucas9621@gmail.com', 'fernanda8358gmail.com', 'joao123@gmail.com', 'pedro2386@', 'bruno8689@', 'spam_carlos5065@gmail.com', 'pedro6178@gmail.com', 'carlos4240@gmail.com', 'joao123@gmail.com', 'julia6333@hotmail.com', 'rafael3673@gmail.com', '$$$lucas3133@gmail.com', 'fernanda1120outlook.com', 'joao123@gmail.com', 'pedro2252@yahoo.com', 'rafael5076@hotmail.com', 'joao123@gmail.com', 'spam_carlos2917@gmail.com', 'ana7919@gmail.com', 'spam_ana2731@gmail.com', 'spam_fernanda805@gmail.com', 'julia2426@', 'ana3164@', 'fernanda4456@outlook.com', 'spam_ana4109@gmail.com', '$$$maria8426@gmail.com', 'joao3132@hotmail.com', 'ana4464@outlook.com', '$$$lucas9208@gmail.com', 'julia4443@', 'spam_carlos2796@gmail.com', 'spam_carlos9201@gmail.com', 'fernanda4570@yahoo.com', 'fernanda446@hotmail.com', 'carlos7643@gmail.com', 'rafael6674@gmail.com', 'ana8903@gmail.com', 'lucas2219@', 'rafael8496@outlook.com', '$$$pedro3901@gmail.com', 'spam_lucas7292@gmail.com', 'pedro2231yahoo.com', 'joao1129@hotmail.com', 'fernanda8017@hotmail.com', 'rafael1419gmail.com', 'maria688outlook.com', 'maria7135@', 'joao7315@', 'rafael9743@yahoo.com', 'spam_rafael7327@gmail.com', '$$$pedro1159@gmail.com', 'joao9821@gmail.com', 'lucas7085@outlook.com', 'bruno6457gmail.com', 'carlos8761@outlook.com', 'pedro3349@yahoo.com', 'carlos4899@', 'bruno7431@yahoo.com', 'spam_joao9825@gmail.com', 'pedro7525@outlook.com', 'joao123@gmail.com', 'pedro5133@gmail.com', 'maria6278@gmail.com', 'fernanda3497@hotmail.com', 'ana2286@gmail.com', 'lucas2761@outlook.com', 'pedro697gmail.com', 'spam_rafael2954@gmail.com', 'fernanda473@yahoo.com', 'julia1444@gmail.com', '$$$julia6582@gmail.com', 'pedro2353@hotmail.com', 'pedro3776@', 'rafael3095outlook.com', 'bruno1285@outlook.com', '$$$bruno3521@gmail.com', 'ana498gmail.com', 'maria1858@gmail.com', 'pedro4384@yahoo.com', 'pedro2842@outlook.com', 'joao123@gmail.com', 'carlos2749@outlook.com', 'spam_joao4907@gmail.com', 'carlos81gmail.com', 'julia3623@hotmail.com', 'rafael5474@', '$$$joao3769@gmail.com', 'joao8739gmail.com', 'carlos2519@hotmail.com', 'bruno9781@hotmail.com', 'joao4774outlook.com', 'carlos2418outlook.com', 'carlos8141@outlook.com', 'lucas9520@yahoo.com', '$$$ana2733@gmail.com', 'carlos6584@hotmail.com', 'joao123@gmail.com', 'maria5657@hotmail.com', 'carlos694hotmail.com', 'joao123@gmail.com', 'bruno5555@', 'ana4460@gmail.com', 'pedro6017@hotmail.com', 'julia4059hotmail.com', 'joao123@gmail.com', '$$$maria9209@gmail.com', 'julia6719@outlook.com', 'rafael9094@outlook.com', 'joao8266@yahoo.com', 'rafael7772@outlook.com', 'joao2371@', 'fernanda9373@outlook.com', 'joao123@gmail.com', 'fernanda6054@gmail.com', 'pedro1846@hotmail.com', '$$$maria5074@gmail.com', 'joao8229@gmail.com', '$$$pedro33@gmail.com', 'maria6812@hotmail.com', 'pedro5816@hotmail.com', 'joao123@gmail.com', 'lucas5991@yahoo.com', 'pedro9335@gmail.com', 'lucas8988hotmail.com', 'ana2376@gmail.com', 'bruno402@', 'ana8228@yahoo.com', 'julia8046@outlook.com', 'joao123@gmail.com', 'pedro9111@gmail.com', '$$$rafael6546@gmail.com', 'rafael3166@gmail.com', 'joao9244hotmail.com', 'carlos5245@hotmail.com', 'julia8261@gmail.com', '$$$pedro9953@gmail.com', 'joao123@gmail.com', 'ana4367outlook.com', 'spam_fernanda82@gmail.com', '$$$carlos5461@gmail.com', 'joao3879@outlook.com', '$$$rafael5987@gmail.com', 'spam_maria8441@gmail.com', 'pedro7292@', '$$$maria1279@gmail.com', 'joao123@gmail.com', 'joao123@gmail.com', 'maria7039outlook.com', 'rafael4172gmail.com', 'carlos7776@yahoo.com', 'pedro1830@', '$$$lucas2809@gmail.com', 'julia4492gmail.com', '$$$bruno4338@gmail.com', 'julia2970@hotmail.com', 'pedro1980@', 'maria9453@', 'joao123@gmail.com', 'bruno1@gmail.com', 'joao123@gmail.com', 'pedro2828@hotmail.com', 'joao9491@yahoo.com', 'fernanda8669@outlook.com', 'pedro3752@hotmail.com', 'lucas8238gmail.com', 'bruno9473@yahoo.com', 'pedro8714@hotmail.com', 'pedro4664@outlook.com', 'carlos8806hotmail.com', 'pedro2306@outlook.com', '$$$maria4931@gmail.com', 'spam_maria259@gmail.com', 'rafael1810@gmail.com', 'fernanda5238@gmail.com', 'fernanda5503@gmail.com', '$$$fernanda8431@gmail.com', 'ana7399@gmail.com', 'lucas5692@gmail.com', '$$$julia6082@gmail.com', 'spam_carlos5449@gmail.com', 'carlos6645@outlook.com', 'fernanda2349@hotmail.com', 'carlos2206@yahoo.com', 'julia3753@', 'rafael7097@hotmail.com', 'rafael276@gmail.com', 'lucas4758@gmail.com', 'joao8917@outlook.com', 'lucas3375@outlook.com', 'rafael9955gmail.com', 'fernanda8297@yahoo.com', 'bruno3894@gmail.com', '$$$julia5057@gmail.com', 'joao123@gmail.com', 'fernanda2712@hotmail.com', 'fernanda2057@gmail.com', 'ana5273outlook.com', 'julia9172@', 'fernanda7435@gmail.com', 'lucas1225@outlook.com', 'rafael8480@gmail.com', 'joao123@gmail.com', '$$$pedro5631@gmail.com', 'spam_carlos1368@gmail.com', 'ana189@yahoo.com', 'ana1582yahoo.com', 'carlos9194@outlook.com', 'ana5119hotmail.com', 'joao233@yahoo.com', 'pedro7274@outlook.com', 'maria1959@yahoo.com', 'joao8004@hotmail.com', 'fernanda8199@outlook.com', 'joao1839@outlook.com', 'lucas4884@outlook.com', 'joao123@gmail.com', '$$$maria7063@gmail.com', 'bruno7404@', 'carlos7933@outlook.com', 'bruno1480@outlook.com', 'rafael8847@', '$$$pedro5643@gmail.com', '$$$pedro3557@gmail.com', 'carlos2364@', 'pedro9935@hotmail.com', 'bruno1455@hotmail.com', 'bruno5348@', 'spam_fernanda6936@gmail.com', 'fernanda9219outlook.com', 'fernanda6676@', 'maria5135@', 'pedro8166@outlook.com', 'joao123@gmail.com', 'spam_carlos2901@gmail.com', 'carlos5371@yahoo.com', 'ana4899@', 'julia1434yahoo.com', 'carlos2092@gmail.com', 'joao8409@gmail.com', 'pedro747@', 'pedro8275@gmail.com', 'maria8418@hotmail.com', 'ana8419@gmail.com', 'bruno5707@gmail.com', 'joao123@gmail.com', 'lucas7880@gmail.com', 'bruno2745yahoo.com', '$$$julia3836@gmail.com', 'spam_rafael6100@gmail.com', 'ana8229@gmail.com', 'maria4714@outlook.com', 'bruno4937@gmail.com', '$$$lucas5688@gmail.com', '$$$julia7600@gmail.com', 'spam_ana5481@gmail.com', 'joao9719@', 'rafael7582@gmail.com', 'joao123@gmail.com', 'julia8482@gmail.com', 'rafael4399@gmail.com', 'rafael9533@yahoo.com', 'rafael9206@', 'ana6795@yahoo.com', 'joao123@gmail.com', 'spam_fernanda1834@gmail.com', '$$$carlos4719@gmail.com', 'joao123@gmail.com', '$$$julia4159@gmail.com', 'lucas3789@yahoo.com', 'joao4754@outlook.com', 'carlos581@hotmail.com', 'joao123@gmail.com', 'bruno2973@hotmail.com', 'joao1705@yahoo.com', 'fernanda7341@hotmail.com', 'carlos5081@gmail.com', 'maria3880gmail.com', 'lucas799@hotmail.com', 'pedro5253@gmail.com', 'bruno3003@hotmail.com', 'maria9442@yahoo.com', 'julia2421@', 'julia385@hotmail.com', 'pedro7610@', 'bruno578@hotmail.com', 'rafael3859@yahoo.com', 'bruno4180@hotmail.com', 'fernanda2028@yahoo.com', 'julia7644@hotmail.com', 'bruno8510@hotmail.com', 'joao123@gmail.com', 'ana4404@', 'spam_maria1442@gmail.com', 'maria7484@hotmail.com', 'joao8945@gmail.com', 'carlos9357@hotmail.com', 'joao123@gmail.com', 'fernanda1274@yahoo.com', 'joao8617@gmail.com', 'pedro4340@gmail.com', 'carlos9269@hotmail.com', 'ana8560@yahoo.com', '$$$bruno6709@gmail.com', 'julia853@outlook.com', 'joao123@gmail.com', 'ana6099@yahoo.com', 'carlos463@gmail.com', '$$$lucas1052@gmail.com', 'spam_joao1921@gmail.com', 'joao123@gmail.com', 'lucas571@hotmail.com', 'bruno4968@', 'rafael7038@outlook.com', 'ana9254outlook.com', 'joao123@gmail.com', 'joao123@gmail.com', 'joao123@gmail.com', 'spam_lucas4454@gmail.com', 'carlos9389@', 'lucas9025@hotmail.com', 'joao123@gmail.com', 'ana2373@hotmail.com', 'lucas2003yahoo.com', '$$$julia4228@gmail.com', 'pedro8020@yahoo.com', 'joao123@gmail.com', 'julia148@outlook.com', 'spam_fernanda6099@gmail.com', 'carlos4168@yahoo.com', '$$$lucas9671@gmail.com', 'rafael6291@yahoo.com', 'bruno5262gmail.com', 'maria8506@hotmail.com', 'fernanda986@gmail.com', 'fernanda9279@outlook.com', 'carlos839gmail.com', '$$$ana1147@gmail.com', 'bruno3965@outlook.com', 'spam_carlos9935@gmail.com', 'joao123@gmail.com', 'joao123@gmail.com', 'spam_fernanda8104@gmail.com', 'joao123@gmail.com', 'maria5349@gmail.com', 'joao123@gmail.com', 'rafael5894@hotmail.com', 'lucas3519@', 'carlos3143@yahoo.com', 'pedro15@hotmail.com', 'rafael3825@gmail.com', 'ana6158@hotmail.com', '$$$carlos3054@gmail.com', 'carlos4940@outlook.com', '$$$lucas5328@gmail.com', 'maria8067@hotmail.com', 'lucas1039@gmail.com', 'joao123@gmail.com', 'rafael1257@gmail.com', 'carlos6136@hotmail.com', 'carlos5793@yahoo.com', 'joao123@gmail.com', 'maria3774@outlook.com', 'fernanda4524gmail.com', 'joao123@gmail.com', '$$$fernanda3916@gmail.com', 'pedro7740@outlook.com', '$$$bruno582@gmail.com', 'joao123@gmail.com', 'spam_maria796@gmail.com', 'joao7712@outlook.com', '$$$bruno5959@gmail.com', 'fernanda7400@hotmail.com', 'julia8417@hotmail.com', 'ana7287@yahoo.com', 'maria6513yahoo.com', 'fernanda9723@hotmail.com', 'joao123@gmail.com', 'fernanda7532outlook.com', '$$$ana6373@gmail.com', 'carlos7382hotmail.com', 'julia705@', 'maria3683@hotmail.com', '$$$lucas4235@gmail.com', 'spam_rafael6671@gmail.com', 'fernanda5950gmail.com', 'lucas9415@', 'joao123@gmail.com', 'pedro3621@gmail.com', 'ana8220@hotmail.com', 'joao123@gmail.com', 'carlos5841@gmail.com', '$$$pedro6202@gmail.com', 'carlos1813@hotmail.com', '$$$lucas4477@gmail.com', 'joao9084@yahoo.com', 'fernanda9023@hotmail.com', 'rafael5841@gmail.com', 'ana6979@yahoo.com', 'rafael4585@gmail.com', 'carlos368@yahoo.com', 'ana1620@', 'ana4667@yahoo.com', 'ana8849@yahoo.com', 'spam_bruno2253@gmail.com', 'lucas2417@yahoo.com', 'ana1772yahoo.com', 'bruno2228@hotmail.com', 'lucas1701@hotmail.com', 'ana2916@yahoo.com', 'carlos9887@hotmail.com', '$$$bruno9668@gmail.com', 'julia8676@hotmail.com', 'ana1588outlook.com', 'carlos8957@hotmail.com', 'bruno2043@outlook.com', 'bruno3103@', 'joao123@gmail.com', '$$$bruno6574@gmail.com', 'maria1265@', 'maria2539gmail.com', 'spam_pedro4121@gmail.com', 'fernanda7324@outlook.com', 'bruno401@hotmail.com', '$$$carlos3149@gmail.com', 'pedro8418@hotmail.com', 'fernanda1087@hotmail.com', 'rafael4329@hotmail.com', '´´rafael4329@hotmail.com']
+
+import string
+
+caracteres_especial = tuple(string.punctuation)
+letras = tuple(string.ascii_letters)
+
+# vou pegar quantos email não tratados tem 
+tamanho_gmail_sujo = len(gmail_sujos)
+print(f"O tamanho total de gmails: {len(gmail_sujos)} gmails")
+
+#gmail com @ e sem
+gmail_com = [gmail for gmail in gmail_sujos if "@" in gmail]
+print(f"Quantidades de gmail com o @: {len(gmail_com)}")
+
+gmail_sem = [gmail for gmail in gmail_sujos if not "@" in gmail]
+print(f"Quantidades de gmail sem o @: {len(gmail_sem)}")
+
+#vamos trabalhar e  fazer uma limpeza mais limpa dos gmails
+gmail_com_caracteres_especial = []
+gmail_com_letras = []
+gmail_com_letras_com = []
+gmail_com_letras_sem = []
+
+for gmail in gmail_com:
+
+    if gmail.startswith(letras) and gmail.endswith(".com"):
+        gmail_com_letras_com.append(gmail)
+
+    if gmail.startswith(letras) and not gmail.endswith(".com"):
+        gmail_com_letras_sem.append(gmail)
+
+
+    if gmail.startswith(letras):
+        gmail_com_letras.append(gmail)
+
+
+    else:
+        gmail_com_caracteres_especial.append(gmail)
+
+
+print(f" tamanho de gmails com letras normais no inicio: {len(gmail_com_letras)}")
+print(f" tamanho de gmails com letras normais no inicio e com o .com no final: {len(gmail_com_letras_com)}")
+print(f" tamanho de gmails com letras normais no inicio e sem o .com no final: {len(gmail_com_letras_sem)}")
+print(f" tamanho de gmails com letras anormais no inicio: {len(gmail_com_caracteres_especial)}")
+
+#agora pegar os span
+gmail_span = []
+gmail_sem_span = []
+
+for gmail in gmail_com_letras_com:
+
+    if "spam" in gmail:
+        gmail_span.append(gmail)
+    else:
+        gmail_sem_span.append(gmail)
+
+print(f" quantidades de gmail de spam {len(gmail_span)}")
+print(f" quantidades de gmail de normal {len(gmail_sem_span)}")
+
+#passo final é pegar as extensão final
+gmail_validos = []
+yahoo_validos = []
+hotmail_validos = []
+outlook_validos = []
+
+for gmail in gmail_sem_span:
+
+    partes = gmail.split("@")
+    dominio = partes[1]
+
+    if dominio == "gmail.com" :
+        gmail_validos.append(gmail)
+
+    elif dominio == "yahoo.com" :
+        yahoo_validos.append(gmail)
+
+    elif dominio == "hotmail.com" :
+        hotmail_validos.append(gmail)
+
+    else:
+        outlook_validos.append(gmail)
+
+#valores unicos
+gmail_unicos = list(set(gmail_validos))
+yahoo_unicos = list(set(yahoo_validos))
+hotmail_unicos = list(set(hotmail_validos))
+outlook_unicos = list(set(outlook_validos))
+
+print(f"quantidades de gmail unicos: {len(gmail_unicos)}")
+print(f"quantidades de yahoo unicos: {len(yahoo_unicos)}")
+print(f"quantidades de hotmail unicos: {len(hotmail_unicos)}")
+print(f"quantidades de outlook unicos: {len(outlook_unicos)}")
+
+
+
+print(f"Quantidades de gmail validados corretamente são: {len(gmail_validos)}")
+print(f"Quantidades de yahoo validados corretamente são: {len(yahoo_validos)}")
+print(f"Quantidades de hotmail validados corretamente são: {len(hotmail_validos)}")
+print(f"Quantidades de outlook validados corretamente são: {len(outlook_validos)}")
+
+    
+
+
+
+
+
+
+
+    
+
