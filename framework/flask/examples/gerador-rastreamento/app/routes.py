0cd6daf1a3c7c48859b3386163db82204526304c
LucianoRamosAlves <141687028+LucianoRamosAlves@users.noreply.github.com>
2026-07-09 22:41:52 -0300

    consultas

--- framework/flask/examples/gerador-rastreamento/app/routes.py
+++ framework/flask/examples/gerador-rastreamento/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app.services.gerador_codigo import gerar_codigo
-from app.models.tracking_code import TrackingCode, TrackingCode
+from app.models.tracking_code import TrackingCode
 from app.extensions import db
 
 def register_routes(app):
@@ -22,3 +22,20 @@ def register_routes(app):
             mensagem = "Código gerado com sucesso!"
 
         return render_template("index.html", codigo=codigo, mensagem=mensagem)
+
+    @app.route("/consultas", methods=["GET", "POST"])
+    def consultas():
+        codigo = None
+        mensagem = None
+
+        if request.method == "POST":
+            codigo = request.form.get("codigo")
+ 
+            registro = TrackingCode.query.filter_by(codigo=codigo).first()
+
+            if registro:
+                mensagem = f"Status: {registro.status} \n Criado em: {registro.criado_em}"
+            else:
+                mensagem = "Código não encontrado."
+
+        return render_template("consultas.html", codigo=codigo, mensagem=mensagem)
