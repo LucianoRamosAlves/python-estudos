a74575894f8885254b295080d806e11aa6799610
LucianoRamosAlves <141687028+LucianoRamosAlves@users.noreply.github.com>
2026-09-03 09:59:27 -0300

    logica viws

--- framework/django/examples/cinepost/filmes/views.py
+++ framework/django/examples/cinepost/filmes/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import PostFilme
+
+
+def post_list(request):
+    posts = PostFilme.publicados.all()
+
+    return render(
+        request,
+        "filmes/post/list.html",
+        {"posts": posts},
+    )
+
+
+def post_detail(request, id):
+    post = get_object_or_404(
+        PostFilme.publicados,
+        id=id,
+        # post=get_object_or_404(
+        #     PostFilme,
+        #     id=id,
+        #     status=PostFilme.Status.PUBLICADO, a gente esta usand o nosso manager acima
+        # ),
+    )
+
+    return render(
+        request,
+        "filmes/post/detail.html",
+        {"post": post},
+    )
